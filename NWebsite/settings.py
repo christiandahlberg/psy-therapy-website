4c7177b6673b0aa4b1e93e3122eabbb7e5859939
Christian Dahlberg <ch7040da-s@student.lu.se>
2019-06-28 15:15:54 +0200

    settings final

--- NWebsite/settings.py
+++ NWebsite/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = get_env_variable('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://nonnywebs.herokuapp.com/', '127.0.0.1']
 
 
 # Application definition
