a10ba6aec841fc9d4fd22e8ae88ae9cc3c8050ae
Christian Dahlberg <ch7040da-s@student.lu.se>
2019-06-28 12:33:18 +0200

    s

--- NWebsite/settings.py
+++ NWebsite/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-from secret_settings import *
+from . import secret_settings
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# from secret_settings import *
+SECRET_KEY = secret_settings.SEC_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
