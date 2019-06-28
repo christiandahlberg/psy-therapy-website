f7b8824f941397ef48ce936503a05ff612c1e64d
Christian Dahlberg <ch7040da-s@student.lu.se>
2019-06-28 13:58:17 +0200

    added production files

--- NWebsite/settings.py
+++ NWebsite/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -124,3 +125,8 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "psychotherapy", "static")
 
 TEMPLATE_DIRS = ( os.path.join(BASE_DIR, "psychotherapy", "templates"), )
+
+# Heroku: Update database configuration from $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn NWebsite.wsgi --log-file -
