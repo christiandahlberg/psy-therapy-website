9e3e2cf8527c5e81edc9d09ec8e0b9e1d8453fc3
Christian Dahlberg <ch7040da-s@student.lu.se>
2019-07-10 10:42:58 +0200

    fix domain

--- NWebsite/urls.py
+++ NWebsite/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('psychotherapy/', include('psychotherapy.urls')),
+    path('', include('psychotherapy.urls')),
     path('admin/', admin.site.urls),
 ]
