dab787e1f01e6ea282a89ba1839b1d9401ce9a8c
Christian Dahlberg <ch7040da-s@student.lu.se>
2019-07-10 10:54:41 +0200

    Text fix

--- NWebsite/settings.py
+++ NWebsite/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = get_env_variable('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['nonnywebs.herokuapp.com', 'nonnywebs.pythonanywhere.com', '127.0.0.1']
 
