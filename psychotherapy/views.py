66651cdd32c3cbdf46b3ca37b9985dd62e29e7ff
Christian Dahlberg <ch7040da-s@student.lu.se>
2019-07-31 14:00:17 +0200

    Re

--- psychotherapy/views.py
+++ psychotherapy/views.py
@@ -2,21 +2,21 @@ from django.shortcuts import render
 from .models import Prices, About, Frontpage
 
 
-ABOUT_PAGE = About.objects.all()
-PRICES_PAGE = Prices.objects.all()
-FRONT_PAGE = Frontpage.objects.all()
+#ABOUT_PAGE = About.objects.all()
+#PRICES_PAGE = Prices.objects.all()
+#FRONT_PAGE = Frontpage.objects.all()
 
 def index(request):
     """ Home """
-    return render(request, 'psychotherapy/index.html', {'front_page': FRONT_PAGE})
+    return render(request, 'psychotherapy/index.html')
 
 def therapy(request):
     """ Shows information regarding this specific sort of therapy."""
-    return render(request, 'psychotherapy/pages/therapy.html', {'about_page': ABOUT_PAGE})
+    return render(request, 'psychotherapy/pages/therapy.html')
 
 def getstarted(request):
     """ Shows contact information and a form to contact."""
-    return render(request, 'psychotherapy/pages/contact.html', {'prices_page': PRICES_PAGE})
+    return render(request, 'psychotherapy/pages/contact.html')
 
 def about(request):
     """ Shows information about the company."""
