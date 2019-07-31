b7fd315156241bab913776c067a8742d350afb44
Christian Dahlberg <ch7040da-s@student.lu.se>
2019-07-31 14:20:14 +0200

    DjangoAdmin last push

--- psychotherapy/views.py
+++ psychotherapy/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 from .models import Prices, About, Frontpage
 
 
-ABOUT_PAGE = About.objects.all()
-PRICES_PAGE = Prices.objects.all()
+ABOUT_PAGE = About.objects.filter()[:1].get()
+PRICES_PAGE = Prices.objects.filter()[:1].get()
 FRONT_PAGE = Frontpage.objects.filter()[:1].get()
 
 def index(request):
@@ -12,11 +12,11 @@ def index(request):
 
 def therapy(request):
     """ Shows information regarding this specific sort of therapy."""
-    return render(request, 'psychotherapy/pages/therapy.html')
+    return render(request, 'psychotherapy/pages/therapy.html', {"about_page": ABOUT_PAGE})
 
 def getstarted(request):
     """ Shows contact information and a form to contact."""
-    return render(request, 'psychotherapy/pages/contact.html')
+    return render(request, 'psychotherapy/pages/contact.html', {"prices_page": PRICES_PAGE})
 
 def about(request):
     """ Shows information about the company."""
