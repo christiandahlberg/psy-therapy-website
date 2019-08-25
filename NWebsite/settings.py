c5c47495bc12615cb306f32e6a55cafdd1734949
Christian Dahlberg <ch7040da-s@student.lu.se>
2019-08-25 21:28:57 +0800

    Add button

--- NWebsite/settings.py
+++ NWebsite/settings.py
@@ -75,6 +75,13 @@ TEMPLATES = [
     },
 ]
 
+TEMPLATE_LOADERS = (
+    ('django.template.loaders.cached.Loader', (
+        'django.template.loaders.filesystem.Loader',
+        'django.template.loaders.app_directories.Loader',
+    )),
+)
+
 WSGI_APPLICATION = 'NWebsite.wsgi.application'
 
 
